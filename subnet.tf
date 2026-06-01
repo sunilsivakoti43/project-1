46f678b98edd1c4bbba2a1a95a24621ee49007e8
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-02 00:49:44 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "swiggy-pub-sub-1" {
 resource "aws_subnet" "swiggy-pub-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.16/28"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = "true"
   tags = {
     Name = "swiggy-pub-sub-2"
@@ -34,7 +34,7 @@ resource "aws_subnet" "swiggy-pvt-sub-1" {
 resource "aws_subnet" "swiggy-pvt-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.48/28"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-2"
@@ -53,7 +53,7 @@ resource "aws_subnet" "swiggy-pvt-sub-3" {
 resource "aws_subnet" "swiggy-pvt-sub-4" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.80/28"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "swiggy-pvt-sub-4"
