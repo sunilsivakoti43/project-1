2c6c5c902ea61b8fd8864fae8c64ac754af2e16c
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-01 23:06:04 +0530

    Add files via upload

---
+++ appasg.tf
@@ -0,0 +1,44 @@
+# Create an EC2 Auto Scaling Group - app
+resource "aws_autoscaling_group" "swiggy-app-asg" {
+  name = "swiggy-app-asg"
+
+  launch_template {
+    id      = aws_launch_template.swiggy-app-template.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = [
+    aws_subnet.swiggy-pvt-sub-1.id,
+    aws_subnet.swiggy-pvt-sub-2.id
+  ]
+
+  min_size         = 2
+  max_size         = 3
+  desired_capacity = 2
+}
+
+# Create a launch template for the EC2 instances
+resource "aws_launch_template" "swiggy-app-template" {
+  name_prefix   = "swiggy-app-template"
+  image_id      = "ami-07a00cf47dbbc844c"
+  instance_type = "t3.micro"
+  key_name      = "kubeadmm"
+
+  network_interfaces {
+    security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
+    associate_public_ip_address = false
+  }
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+
+    sudo yum install mysql -y
+  EOF
+  )
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
+}
+
