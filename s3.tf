6726b15d70eadfa3e944e322ecab3ca97447f937
sunilsivakoti43 <sunilsivakoti43@gmail.com>
2026-06-02 16:55:07 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform-0099-sunil45678"
+  bucket = "swiggy-bucket-terraform-0099-sunil4567844560"
 
   # Enable versioning
   versioning {
